7613809c26d8a2cccc128bea6005f4951e5549e2
Gabriel Ramos <gcrepho@gmail.com>
2026-05-18 15:01:55 -0300

    chore: init SvelteKit with adapter-static and bun

---
+++ svelte.config.js
@@ -0,0 +1,14 @@
+import adapter from '@sveltejs/adapter-static';
+
+/** @type {import('@sveltejs/kit').Config} */
+export default {
+  kit: {
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: undefined,
+      precompress: false,
+      strict: true
+    })
+  }
+};
