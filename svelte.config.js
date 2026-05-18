ab9b5f667028c1f2228bff64d49d67631c615bd1
Gabriel Ramos <gcrepho@gmail.com>
2026-05-18 17:22:22 -0300

    fix(config): add vitePreprocess to svelte.config.js for VSCode language server support

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,9 @@
 import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 export default {
+  preprocess: vitePreprocess(),
   kit: {
     adapter: adapter({
       pages: 'build',
